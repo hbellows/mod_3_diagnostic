ca7e95e7659eebdd2e68d716f3e0dc880d09843e
Harper Bellows <harper.bellows@gmail.com>
2018-12-12 10:24:07 -0700

    add passing service test

---
+++ spec/services/nrel_service_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe NrelService do
+  it 'can retrieve station data' do
+    service = NrelService.new
+    station_data = service.get_stations('80203')
+
+    expect(station_data).to be_a(Hash)
+    expect(station_data[:fuel_stations]).to be_a(Array)
+    expect(station_data[:fuel_stations][0]).to have_key(:station_name)
+    expect(station_data[:fuel_stations][0]).to have_key(:street_address)
+    expect(station_data[:fuel_stations][0]).to have_key(:fuel_type_code)
+    expect(station_data[:fuel_stations][0]).to have_key(:distance)
+    expect(station_data[:fuel_stations][0]).to have_key(:access_days_time)
+    expect(station_data[:fuel_stations][0][:station_name]).to be_a(String)
+    expect(station_data[:fuel_stations][0][:street_address]).to be_a(String)
+    expect(station_data[:fuel_stations][0][:fuel_type_code]).to be_a(String)
+    expect(station_data[:fuel_stations][0][:distance]).to be_a(Float)
+    expect(station_data[:fuel_stations][0][:access_days_time]).to be_a(String)
+  end
+end
+
+  # :station_name=>"UDR",
+  # :street_address=>"800 Acoma St",
+  # :fuel_type_code=>"ELEC",
+  # :distance=>0.31422,
+  # :access_days_time=>"24 hours daily"
