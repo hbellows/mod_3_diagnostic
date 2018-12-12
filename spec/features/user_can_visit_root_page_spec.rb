c76df70fd5269d2e17785f9e87bdefd6dcfe7ebc
Harper Bellows <harper.bellows@gmail.com>
2018-12-12 09:20:54 -0700

    add skeleton of feature test

--- spec/features/user_can_visit_root_page_spec.rb
+++ spec/features/user_can_visit_root_page_spec.rb
@@ -8,4 +8,24 @@ feature "User can visit root page" do
       expect(page).to have_selector("input[value='Search by zip...']")
     end
   end
+
+  scenario 'and enters a zip code in the search box' do
+    it 'displays 10 closest stations and each station\'s details' do
+      
+      visit '/'
+      
+      fill_in :q, with '80203'
+
+      click_on 'Locate'
+
+      expect(current_path).to eq('/search')
+
+      expect(page).to have_content('Nearest Stations')
+      expect(page).to have_content('Name: ')
+      expect(page).to have_content('Address: ')
+      expect(page).to have_content('Fuel Types: ')
+      expect(page).to have_content('Distance: ')
+      expect(page).to have_content('Access Times: ')
+    end
+  end
 end
