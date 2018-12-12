9c7978471fd511e47ec10657c80ea19ceea38fcd
Harper Bellows <harper.bellows@gmail.com>
2018-12-12 09:45:28 -0700

    add first dive down to service to set up connection and get request - research param requirements

---
+++ app/services/nrel_service.rb
@@ -0,0 +1,20 @@
+class NrelService
+
+  def get_stations(zip_code, radius = 6)
+    get_json("/api/alt-fuel-stations/v1/nearest.json?")
+  end
+
+  private
+
+  def conn
+    Faraday.new(url: "URL") do |faraday|
+      faraday.params["api_key"] = ENV["NREL_API_KEY"]
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+  
+  def get_json(url)
+    JSON.parse(conn.get(url).body, symbolize_names: true)
+  end
+
+end
