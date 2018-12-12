58aa15d7e19df59115832fd8d14fd47036cdac4b
Harper Bellows <harper.bellows@gmail.com>
2018-12-12 09:56:33 -0700

    find and pull out likely attributes from json response

--- app/facades/station_search_facade.rb
+++ app/facades/station_search_facade.rb
@@ -5,10 +5,17 @@ class StationSearchFacade
   end
 
   def stations
-    station_data.map do |station|
+    binding.pry
+    station_data[:fuel_stations].map do |station|
       Station.new(station)
     end
   end
+
+  # :station_name=>"UDR",
+  # :street_address=>"800 Acoma St",
+  # :fuel_type_code=>"ELEC",
+  # :distance=>0.31422,
+  # :access_days_time=>"24 hours daily"
 
   private
 
@@ -19,5 +26,4 @@ class StationSearchFacade
   def station_data
     nrel_service.get_stations(@zip_code)
   end
-
 end
